ec963e4107d37630fcd19b73171dd065ea8b5879
EoSubin <2023111537@dgu.ac.kr>
2024-11-19 23:25:05 +0900

    first commit

---
+++ project/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.shortcuts import redirect
+from django.conf import settings  # 추가
+from django.conf.urls.static import static
+from .views import home_view
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('weather/', include('weather.urls')),  
+    path('users/', include('users.urls')),
+    path('survey/', include('survey.urls')),
+    path('feedback/', include('feedback.urls')),
+    path('', home_view, name='home'),  # 초기화면을 홈 화면 뷰로 변경
+]
+
+# 미디어 파일 제공 설정 (DEBUG=True 환경에서만)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
---
+++ users/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import UserProfile
+from .forms import SignUpForm, LoginForm
+
+
+
+# 회원가입 뷰
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            UserProfile.objects.create(user=user)  # UserProfile 생성
+            login(request, user)  # 자동 로그인
+            return redirect('/feedback/')  # 리다이렉트 URL
+    else:
+        form = SignUpForm()
+    return render(request, 'users/signup.html', {'form': form})
+
+
+
+# 로그인 뷰
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            # `next` 파라미터 확인 후 없으면 기본 URL로 리다이렉트
+            next_url = request.GET.get('next', '/feedback/')
+            return redirect(next_url)
+    else:
+        form = LoginForm()
+    return render(request, 'users/login.html', {'form': form})
+
+
+# 로그아웃 뷰
+def logout_view(request):
+    logout(request)
+    return redirect('home')
+
+# OOTD 사진 업로드 뷰
+@login_required
+def upload_ootd_view(request):
+    if request.method == 'POST' and 'ootd_photo' in request.FILES:
+        photo = request.FILES['ootd_photo']
+        profile = request.user.userprofile
+        profile.ootd_photo = photo
+        profile.save()
+        messages.success(request, 'OOTD 사진이 업로드되었습니다.')
+        return redirect('home')
+    return render(request, 'upload_ootd.html')
+
+# url 뷰 함수 생성
+def upload_ootd(request):
+    return render(request, 'users/upload_ootd.html')
